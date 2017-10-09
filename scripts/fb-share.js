6e67b0870f43e7feb31896758baf07f5470baed1
Liam Morgan <lite@getignited.co.uk>
2017-10-09 10:53:41 +0100

    Facebook Share fixes

--- scripts/fb-share.js
+++ scripts/fb-share.js
@@ -1,7 +1,18 @@
-document.getElementById('facebookShare').onclick = function() {
-  FB.ui({
-    method: 'share',
-    mobile_iframe: true,
-    href: '',
-  }, function(response){});
+export default class facebookShare {
+  constructor(el) {
+    el.addEventListener('click', (e) => { this.onClick(e) })
+  }
+
+  onClick(evt) {
+    const facebookLink = document.querySelector('[data-link]')
+    const theLink = facebookLink.dataset.link
+
+    console.log(theLink)
+
+    FB.ui({
+      method: 'share',
+      mobile_iframe: true,
+      href: theLink,
+    }, function(response){});
+  }
 };
